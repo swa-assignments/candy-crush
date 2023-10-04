4e62c6697be97e8fb348c1c563f323a0574a18af
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-10-04 11:58:56 +0200

    fix: combo not allowed on 2 different x axis

--- app.js
+++ app.js
@@ -97,6 +97,9 @@ document.addEventListener('DOMContentLoaded', () => {
             let decidedColor = squares[i].style.backgroundColor
             const isBlank = squares[i].style.backgroundColor === ''
 
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+            if (notValid.includes(i)) continue
+
             if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
                 score += 3
                 //scoreDisplay.innerHTML = score
