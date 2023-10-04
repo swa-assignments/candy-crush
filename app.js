c978e0ec9733f8c78bf7bb8b71f76df77c2c6a7e
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-10-04 11:57:46 +0200

    feat: add implementation for 4 in a row

--- app.js
+++ app.js
@@ -125,6 +125,43 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // Check for row of four
+    function checkRowForFour() {
+        for (let i = 0; i < 60; i++) {
+            let rowOfFour = [i, i+1, i+2, i+3]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
+            if (notValid.includes(i)) continue
+
+            if (rowOfFour.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 4
+                //scoreDisplay.innerHTML = score
+                rowOfFour.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        }
+    }
+
+    // Check for column of four
+    function checkColumnForFour() {
+        for (let i = 0; i < 39; i++) {
+            let columnOfFour = [i, i+width, i+width*2, i+width*3]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            if (columnOfFour.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 4
+                //scoreDisplay.innerHTML = score
+                columnOfFour.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        }
+    }
+
     window.setInterval(function(){
         checkRowForThree()
         checkColumnForThree()
