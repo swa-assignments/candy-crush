ba7fb8c6c13ca3247dddf4ef8fc90dd7cd7efeaa
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-10-04 11:43:39 +0200

    feat: implement logic for color drag & replacement

--- app.js
+++ app.js
@@ -25,4 +25,66 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     createBoard()
+
+    // Drag the candies
+    let colorBeingDragged
+    let colorBeingReplaced
+    let squareIdBeingDragged
+    let squareIdBeingReplaced
+
+    squares.forEach(square => square.addEventListener('dragstart', dragStart))
+    squares.forEach(square => square.addEventListener('dragend', dragEnd))
+    squares.forEach(square => square.addEventListener('dragover', dragOver))
+    squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+    squares.forEach(square => square.addEventListener('dragleave', dragLeave))
+    squares.forEach(square => square.addEventListener('drop', dragDrop))
+
+    function dragStart() {
+        colorBeingDragged = this.style.backgroundColor
+        squareIdBeingDragged = parseInt(this.id)
+        console.log(this.id, 'dragstart')
+    }
+
+    function dragEnd() {
+        console.log(this.id, 'dragend')
+        // What is a valid move?
+        let validMoves = [
+            squareIdBeingDragged - 1,
+            squareIdBeingDragged - width,
+            squareIdBeingDragged + 1,
+            squareIdBeingDragged + width
+        ]
+        let validMove = validMoves.includes(squareIdBeingReplaced)
+
+        if (squareIdBeingReplaced && validMove) {
+            squareIdBeingReplaced = null
+        } else if (squareIdBeingReplaced && !validMove) {
+            squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced
+            squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+        } else {
+            squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+        }
+    }
+
+    function dragOver(e) {
+        e.preventDefault()
+        console.log(this.id, 'dragover')
+    }
+
+    function dragEnter(e) {
+        e.preventDefault()
+        console.log(this.id, 'dragenter')
+    }
+
+    function dragLeave() {
+        console.log(this.id, 'dragleave')
+    }
+
+    function dragDrop() {
+        console.log(this.id, 'dragdrop')
+        colorBeingReplaced = this.style.backgroundColor
+        squareIdBeingReplaced = parseInt(this.id)
+        this.style.backgroundColor = colorBeingDragged
+        squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced
+    }
 })
